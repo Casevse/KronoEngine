0175605d0af347e0b662a4026bffd5432500cfe8
Carlos <csevilas@gmail.com>
2016-09-01 12:40:37 +0200

    Added GLFW 3 with project settings. Added Scene and Entity classes.

--- KronoEngine/KronoEngine/Main.cpp
+++ KronoEngine/KronoEngine/Main.cpp
@@ -1,4 +1,31 @@
-int main() {
+
+#include <GLFW\glfw3.h>
+
+int main(void) {
+	GLFWwindow* mWindow;
+
+	if (!glfwInit()) {
+		return -1;
+	}
+
+	mWindow = glfwCreateWindow(800, 600, "KronoEngine", NULL, NULL);
+
+	if (!mWindow) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(mWindow);
+
+	while (!glfwWindowShouldClose(mWindow)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(mWindow);
+
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 
 	return 0;
 }
