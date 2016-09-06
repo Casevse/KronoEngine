979e48cb7f4d8bbaef00ff6415b028544e0829ab
Carlos <csevilas@gmail.com>
2016-09-06 19:32:21 +0200

    Add PNG texture loading using lodepng.

--- KronoEngine/KronoEngine/Main.cpp
+++ KronoEngine/KronoEngine/Main.cpp
@@ -4,12 +4,14 @@
 Renderer renderer;
 
 void ConfigureScene() {
-	renderer.SetGlobalLightParameters(glm::vec3(-100.0f, 100.0f, -100.0f), 0.4f, glm::ivec4(255, 255, 255, 255), 100.0f);
+	renderer.SetGlobalLightParameters(glm::vec3(-100.0f, 100.0f, -100.0f), 0.8f, glm::ivec4(255, 255, 255, 255), 100.0f);
 
-	renderer.AddCamera(glm::vec3(0.0f, 3.0f, 5.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+	renderer.AddCamera(glm::vec3(0.0f, 2.0f, 4.0f), glm::vec3(0.0f, 0.0f, 0.0f));
 	renderer.AddCube(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::ivec4(255, 255, 0, 255));
 	renderer.AddLight(glm::vec3(2.0f, 2.0f, 2.0f), glm::ivec4(0, 255, 0, 255), 0.5f , 0.5f, 0.5f);
-	renderer.AddMesh("../Media/Meshes/Kedi/kedi-static.km", glm::vec3(2.0f, 0.5f, 0.0f));
+	
+	Node *mesh = renderer.AddMesh("../Media/Meshes/Kedi/kedi-static.km", glm::vec3(2.0f, 0.5f, 0.0f));
+	mesh->SetTexture("../Media/Textures/Kedi/kedi-texture.png");
 
 	Node *plane = renderer.AddPlane(glm::vec3(0.0f, -0.5f, 0.0f), 8, 8);
 	plane->SetColor(glm::ivec4(0, 100, 100, 255));
--- KronoEngine/KronoEngine/Resource/TextureResource.cpp
+++ KronoEngine/KronoEngine/Resource/TextureResource.cpp
@@ -1,9 +1,48 @@
 #include <Resource\TextureResource.h>
 
+#include <lodepng\lodepng.h>
+
 TextureResource::TextureResource(char *path, bool flipped) {
 	mPath = path;
 
-	// TODO load from image using some library.
+	std::vector<unsigned char> image;
+	unsigned int width, height, error;
+	error = lodepng::decode(image, width, height, path);
+
+	// Flip Y
+	unsigned char* pixels = (unsigned char*)malloc(width * height * 4);
+	unsigned int pos = 0;
+
+	for (unsigned int r = 0; r < height; r++) {
+		for (unsigned int b = 0; b < width * 4; b++) {
+			pixels[pos] = image[(height - r - 1) * width * 4 + b];
+			pos++;
+		}
+	}
+
+	mMinMagFilter = GL_LINEAR;
+	mWrapMode = GL_CLAMP_TO_EDGE;
+
+	glActiveTexture(GL_TEXTURE0);
+	glGenTextures(1, &mTextureObject);
+	glBindTexture(GL_TEXTURE_2D, mTextureObject);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, mMinMagFilter);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, mMinMagFilter);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, mWrapMode);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, mWrapMode);
+
+	glTexImage2D(GL_TEXTURE_2D,
+		0,
+		GL_RGBA,
+		width,
+		height,
+		0,
+		GL_RGBA,
+		GL_UNSIGNED_BYTE,
+		pixels);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 TextureResource::TextureResource(char *path, unsigned char *pixels, int width, int height) {
